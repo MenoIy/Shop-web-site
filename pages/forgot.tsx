369b4540afa390a1da131ca5f22af96d1f78c226
menoIy <karim.daou.1993@gmail.com>
2021-10-18 20:39:05 +0100

    add login page with style

---
+++ pages/forgot.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FogotPassword = () => {
+  return <div>forgot password</div>;
+};
+
+export default FogotPassword;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Link from 'next/link';
+
+import styles from '../styles/login.module.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.container}>
+      <form>
+        <div className={styles.input}>
+          <input placeholder="Email" />
+          <p className={styles.error}>Error</p>
+        </div>
+        <div className={styles.input}>
+          <input type="password" placeholder="Password" />{' '}
+          <p className={styles.error}>Error</p>
+        </div>
+        <button type="submit">Login</button>
+        <Link href="/forgot">
+          <a> Forgot password ?</a>
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
