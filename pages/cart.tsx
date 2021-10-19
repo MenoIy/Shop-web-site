531724b04fc0054eb70d1e245ef0f541187eb1bf
menoIy <karim.daou.1993@gmail.com>
2021-10-19 10:36:34 +0100

    add empty cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Link from 'next/link';
+
+import styles from '../styles/cart.module.css';
 
 const Cart = () => {
-  return <div>Shopping Cart</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.emptyCart}>
+        <span className="fas fa-cart-plus" />
+        <p>Your cart is Empty</p>
+        <Link href="/">
+          <a>Continue shoping</a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
