2a7e97c5f2e2000eabf9b9f72f7d81739ada9a50
menoIy <karim.daou.1993@gmail.com>
2021-10-21 14:00:19 +0100

    add check env variable

--- utils/dbConnect.ts
+++ utils/dbConnect.ts
@@ -1,20 +1,21 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL;
-
-if (!MONGODB_URL) {
-  throw new Error(
-    'Please define the MONGODB_URL environment variable inside .env.local'
-  );
-}
-
 async function dbConnect() {
+  const MONGODB_URL = process.env.MONGODB_URL;
+
+  if (!MONGODB_URL) {
+    throw new Error(
+      'Please define the MONGODB_URL environment variable inside .env.local'
+    );
+  }
+
   if (mongoose.connection) {
     console.log('already connected');
     return;
   }
+
   try {
-    mongoose.connect(process.env.DB_URL || '', () =>
+    mongoose.connect(MONGODB_URL, () =>
       console.log('MongoDB connected successfully.')
     );
   } catch (error) {
